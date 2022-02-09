e9cea4984ad7da66a9ac3b3b886f989ad75adda4
Wictor Feitosa Leal Cardoso <wictor.feitosa@gmail.com>
2022-02-08 22:04:16 -0300

    index.js corrigido e alterado.

--- modulo1/modulo1/variaveis/template_javascript/index.js
+++ modulo1/modulo1/variaveis/template_javascript/index.js
@@ -21,30 +21,35 @@ let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
 let recebimentoPorDia = prompt("Quanto você recebe por dia?")
 alert(`Voce recebe ${recebimentoPorDia/horasTrabalhadas} por hora`)*/
 
-
-const meuNome = prompt ("Digite Wictor")
-const minhaIdade = prompt ("Digite 28")
+//1. a), b), c), d)
+let meuNome 
+let minhaIdade 
 console.log(typeof meuNome, typeof minhaIdade)
-// string string foram minhas respostas, devido as aspas duplas que existe no comando prompt internamente.
+// undefined undefined, por não ter nenhum valor atribuido ele retorna indefinido.
 
-const meuNome = prompt ("Digite Wictor")
-const minhaIdade = prompt ("Digite 28")
+//1. e), f)
+let meuNome = prompt ("Digite Wictor")
+let minhaIdade = prompt ("Digite 28")
 console.log(typeof meuNome, typeof minhaIdade)
-// Não vi alteração, continua sendo string e string 
+// string string, teve alteração para tipo de "texto"
 
+// 1. g)
 const meuNome = prompt ("Digite seu nome")
 const minhaIdade = prompt ("Digite sua idade")
 console.log("Olá", meuNome,",", "você tem", minhaIdade, "anos.")
 // Olá Wictor , você tem 28 anos.
 
+// 2. a), b)
 const primeiraResposta = prompt ("Está em casa hoje?")
 const segundaResposta = prompt ("Gosta de refrigerante?")
 const terceiraResposta = prompt ("Está com sono?")
 console.log("Está em casa hoje?", primeiraResposta,", Gosta de refrigerante?", segundaResposta,", Está com sono?", terceiraResposta)
 // Está em casa hoje? não , Gosta de refrigerante? não , Está com sono? não
 
+
+// 3.
 let a = 10
-let b = 25
+let b = 25 // Antes eu havia colocado let c = a e também deu certo
 c = a
 a = b
 b = c
