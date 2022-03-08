80bfa59d023343f675331f88d959384a9c6f22da
Wictor Feitosa Leal Cardoso <wictor.feitosa@gmail.com>
2022-03-08 16:07:48 -0300

    projeto ainda não concluído

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+   console.log("Boas vindas ao jogo de Balckjack")
+    if(confirm("Quer iniciar uma rodada?")) {
+      const carta = comprarCarta();
+      console.log(carta.texto)
+      console.log(carta.valor)
+      
+   } else {
+      console.log("O jogo acabou")
+   }
