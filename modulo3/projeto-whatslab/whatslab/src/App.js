2a99764a38669b7df81cd58ab95303fa6793d742
Wictor Feitosa Leal Cardoso <wictor.feitosa@gmail.com>
2022-05-02 00:41:23 -0300

    mensagens ok

--- modulo3/projeto-whatslab/whatslab/src/App.js
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { MeuPrimeiroComponente } from "./components/MeuPrimeiroComponente";
 import MeuSegundoComponente from "./components/MeuSegundoComponente";
+
+
 import {
   Title,
   Campo,
@@ -9,20 +11,63 @@ import {
   CaixaBaixo,
   InputNome,
   InputMensagem,
+  Mensagem,
 } from "./components/styles";
 
-function App() {
+
+
+class App extends React.Component {
+
+  state = {
+    pessoas: [],
+    valorUsuario : '',
+    valorEmail: ''
+  };
+  
+  adiconarPessoa = () => {
+   const novaPessoa ={
+      nome: this.state.valorUsuario,
+      email: this.state.valorEmail
+    }
+    
+    // Copia array de pessoas e adiciona uma pessoa no final
+    const novoPessoas = [...this.state.pessoas, novaPessoa];
+    
+    // Define o estado "pessoas" como a variavel "novoPessoas"
+    this.setState({ pessoas: novoPessoas });
+  };
+
+  onChangeUsuario = (event) => {
+    this.setState({valorUsuario: event.target.value})
+  }
+  
+  onChangeEmail = (event) => {
+    this.setState({valorEmail: event.target.value})
+  }
+
+  
+render() {
+ const ListaDeComponentes = this.state.pessoas.map((pessoa)=> {
+    return (
+      <p>
+        {pessoa.nome} diz: {pessoa.email}
+      </p>
+    )   
+  });
+
   return (
     <Conteiner>
-      <Campo>
+      <Campo>        
+        <Mensagem>{ListaDeComponentes}</Mensagem>        
         <DentroCampo>
-          <InputNome type="text" placeholder="Usuário"></InputNome>
-          <InputMensagem type="text" placeholder="Mensagem"></InputMensagem>
-          <button>Enviar</button>
+          <InputNome placeholder={"Usuário"} onChange={this.onChangeUsuario} value={this.state.valorUsuario}></InputNome>
+          <InputMensagem placeholder={"Mensagem"} onChange={this.onChangeEmail} value={this.state.valorEmail}></InputMensagem>
+          <button onClick={this.adiconarPessoa}>Enviar</button>          
         </DentroCampo>
       </Campo>
     </Conteiner>
   );
+}
 }
 
 export default App;
--- modulo3/projeto-whatslab/whatslab/src/components/styles.js
+++ modulo3/projeto-whatslab/whatslab/src/components/styles.js
@@ -16,7 +16,11 @@ export const Campo = styled.div`
   border: 1px solid black;
   flex: 1 1 0%;
   display: flex;
-  flex-direction: column-reverse;
+  flex-direction: column;
+  background: #0f0c29;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to bottom, #24243e, #302b63, #0f0c29);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #24243e, #302b63, #0f0c29); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+color: white;
 `;
 
 export const DentroCampo = styled.div`
@@ -26,6 +30,7 @@ export const DentroCampo = styled.div`
 export const InputNome = styled.input`
   width: 100px;
 `;
+
 export const InputMensagem = styled.input`
   flex: 1 1 0%;
 `;
@@ -33,3 +38,12 @@ export const InputMensagem = styled.input`
 export const CaixaBaixo = styled.div`
   display: flex;
 `;
+
+export const Mensagem = styled.p`
+    flex: 1 1 0%;
+    display: flex;
+    flex-direction: column;
+    -webkit-box-pack: end;
+    justify-content: flex-end;
+    padding: 20px;
+`
