e155557ccb87838496284b78470ab3130675317b
WictorFeitosa <wictor.feitosa@gmail.com>
2022-08-04 23:53:10 -0300

    atividade

---
+++ modulo5/node-package-json/index.js
@@ -0,0 +1,6 @@
+// A
+console.log(`Olá, ${process.argv[2]}! Você tem ${process.argv[3]} anos.`)
+
+// B
+console.log(`Olá, ${process.argv[2]}! Você tem ${Number(process.argv[3])} anos. Em sete anos você terá ${Number(process.argv[3]) + Number(7)}`)
+
