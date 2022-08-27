e8232b8ae41c3ce6c3f4295156af43f18bce035d
unknown <wictor.feitosa@gmail.com>
2022-08-26 21:36:24 -0300

    att

--- modulo5/sistema-bancario/src/index.ts
+++ modulo5/sistema-bancario/src/index.ts
@@ -35,60 +35,6 @@ app.post("/users", (req, res) => {
 })
 
 
-
-// // endpoint posta as informações
-// app.post("/post", (req, res) => {
-//   const name = req.body.name;
-//   const price = req.body.price;
-
-//   arrayProducts.findIndex((indice) => {
-//     if (indice.name === name) {
-//       throw "item já existe";
-//     }
-//   });
-
-//   const newArrayProducts: arRay = {
-//     id:uuid(),
-//     name,
-//     price,
-//   };
-//   arrayProducts.push(newArrayProducts);
-//   res.send(arrayProducts);
-// });
-
-
-// app.get("/todosprodutos", (req, res) => {
-//   res.send(arrayProducts);
-// });
-
-
-// // endpoint modifica as informações
-// app.put("/put/:id", (req, res) => {
-//   const newPrice = req.body.price;
-//   const id = req.params.id;
-//   arrayProducts.map((index) => {
-//     if (id === index.id) {
-//       index.price = newPrice;
-//     }
-//     res.send(arrayProducts);
-//   });
-// });
-
-// // endpoint deleta as informações
-// app.delete("/del/:id", (req, res) => {
-//   const id = req.params.id;
-
-//  arrayProducts.filter((index)=>{
-//   if (id === index.id){
-//     arrayProducts.map((index)=>{    
-//        index.id !== id
-//       res.send(index)
-//   }
-// })
-// });
-
-
-
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
     const address = server.address() as AddressInfo;
