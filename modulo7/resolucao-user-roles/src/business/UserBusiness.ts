842162149ebf920d654504857f390551be909a14
unknown <wictor.feitosa@gmail.com>
2022-11-08 20:04:16 -0300

    atividade de roles

---
+++ modulo7/resolucao-user-roles/src/business/UserBusiness.ts
@@ -0,0 +1,153 @@
+import { UserDatabase } from "../data/UserDatabase";
+import {
+  CustomError,
+  InvalidEmail,
+  InvalidName,
+  InvalidPassword,
+  InvalidRole,
+  Unauthorized,
+  UserNotFound,
+} from "../error/customError";
+import {
+  UserInputDTO,
+  user,
+  EditUserInputDTO,
+  EditUserInput,
+  LoginInputDTO,
+  UserRole,
+} from "../model/user";
+import { HashManager } from "../services/HashManager";
+import { IdGenerator, IdGeneratorInterface } from "../services/IdGenerator";
+import { TokenGenerator } from "../services/TokenGenerator";
+
+export class UserBusiness {
+  constructor(
+    private readonly idGenerator: IdGeneratorInterface,
+    private readonly tokenGenerator: TokenGenerator,
+    private readonly hashManager: HashManager,
+    private readonly userDatabase: UserDatabase
+  ) {}
+
+  public createUser = async (input: UserInputDTO): Promise<string> => {
+    try {
+      const { name, nickname, email, password } = input;
+      let role = input.role;
+
+      if (!name || !nickname || !email || !password || !role) {
+        throw new CustomError(
+          400,
+          'Preencha os campos "name", "nickname", "email", "password" e "role"'
+        );
+      }
+
+      if (name.length < 4) {
+        throw new InvalidName();
+      }
+
+      if (!email.includes("@")) {
+        throw new InvalidEmail();
+      }
+
+      // if (role !== "NORMAL" && role !== "ADMIN") {
+      //   throw new InvalidRole();
+      // }
+
+      if (role !== "NORMAL" && role !== "ADMIN") {
+        role = "NORMAL";
+      }
+
+      const id: string = this.idGenerator.generateId();
+
+      const hashPassword: string = await this.hashManager.hash(password);
+
+      const user: user = {
+        id,
+        name,
+        nickname,
+        email,
+        password: hashPassword,
+        role: UserRole[role as keyof typeof UserRole],
+      };
+
+      await this.userDatabase.insertUser(user);
+      const token = this.tokenGenerator.generateToken(id, user.role);
+
+      return token;
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+
+  public login = async (input: LoginInputDTO): Promise<string> => {
+    try {
+      const { email, password } = input;
+
+      if (!email || !password) {
+        throw new CustomError(400, 'Preencha os campos"email" e "password"');
+      }
+
+      if (!email.includes("@")) {
+        throw new InvalidEmail();
+      }
+
+      const user = await this.userDatabase.findUser(email);
+
+      if (!user) {
+        throw new UserNotFound();
+      }
+
+      const isValidPassword: boolean = await this.hashManager.compare(
+        password,
+        user.password
+      );
+
+      if (!isValidPassword) {
+        throw new InvalidPassword();
+      }
+
+      const token = this.tokenGenerator.generateToken(user.id, user.role);
+
+      return token;
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+
+  public editUser = async (input: EditUserInputDTO) => {
+    try {
+      const { name, nickname, id, token } = input;
+
+      if (!name || !nickname || !id || !token) {
+        throw new CustomError(
+          400,
+          'Preencha os campos "id", "name", "nickname" e "token"'
+        );
+      }
+
+      const data = this.tokenGenerator.tokenData(token);
+      // Aqui eu tenho token.role para usar na validação
+
+      if (data.role != UserRole.ADMIN) {
+        throw new Unauthorized();
+      }
+
+      if (!data.id) {
+        throw new Unauthorized();
+      }
+
+      if (name.length < 4) {
+        throw new InvalidName();
+      }
+
+      const editUserInput: EditUserInput = {
+        id,
+        name,
+        nickname,
+      };
+
+      await this.userDatabase.editUser(editUserInput);
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+}
